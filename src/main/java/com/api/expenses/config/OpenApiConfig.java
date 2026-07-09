3c9930d0dab5a3dc77141d7fdd172219270e50e8
Aqif Mahmood <94413200+aky-engineered@users.noreply.github.com>
2026-07-09 01:26:08 +0100

    Adding authentication to swagger

--- src/main/java/com/api/expenses/config/OpenApiConfig.java
+++ src/main/java/com/api/expenses/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.api.expenses.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,7 +13,15 @@ import org.springframework.context.annotation.Configuration;
                 title = "Employee Expense Claim API",
                 description = "Backend API for submitting and approving expense claims",
                 version = "1.0.0"
-        )
+        ),
+        security = @SecurityRequirement(name = "Bearer Auth")
+)
+@SecurityScheme(
+        name = "Bearer Auth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Paste your JWT token obtained from POST /api/auth/login"
 )
 public class OpenApiConfig {
 }
